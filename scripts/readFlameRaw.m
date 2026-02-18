2aaecacd5f7939e32ac7c77acd0675b11afb8613
AlexVallmitjana <antares68@gmail.com>
2026-02-18 12:44:33 -0800

    v1.211

--- scripts/readFlameRaw.m
+++ scripts/readFlameRaw.m
@@ -13,16 +13,16 @@ if(isempty(aux)),error('No tif files!');end
 for ii=1:numel(aux)
     aux1=imfinfo([path filesep aux(ii).name]);
     if(ii>1)
-if(aux1(1).Height~=aux2(1))||(aux1(1).Width~=aux2(2))||(numel(aux1)~=aux2(3))
-error('Images have different sizes!')
-end
+        if(aux1(1).Height~=aux2(1))||(aux1(1).Width~=aux2(2))||(numel(aux1)~=aux2(3))
+            error('Images have different sizes!')
+        end
     end
-    aux2=[aux1(1).Height aux1(1).Width numel(aux1)];    
+    aux2=[aux1(1).Height aux1(1).Width numel(aux1)];
 end
 %%
 txt=dir([path filesep '*Data.txt']);
 if~isempty(txt)
-    [dad] = readScanImageMeta([path filesep txt(1).name]);    
+    [dad] = readScanImageMeta([path filesep txt(1).name]);
 else
     error('No metadata file!');
 end
@@ -50,14 +50,14 @@ aux4=aux0(find(aux0>aux3,1,'first'));
 dad.zZeroUm=str2num(path(aux3+2:aux4-1));
 % size(IT)
 if((id==2)||(id==4))
-            [m,n,success] = getTileDims(path);
-        if(success==1)
-            dad.tileRows=m;
-            dad.tileCols=n;
-        else
-           ensenya('Warning: Mosaic dimensions error','a'); 
-        end
-        try,dad=rmfield(dad,'tileCornerPtsUm');end% that field belongs only to the first tile
+    [m,n,success] = getTileDims(path);
+    if(success==1)
+        dad.tileRows=m;
+        dad.tileCols=n;
+    else
+        ensenya('Warning: Mosaic dimensions error','a');
+    end
+    try,dad=rmfield(dad,'tileCornerPtsUm');end% that field belongs only to the first tile
     if(TT~=size(IT,4))
         TT=size(IT,4);
         ensenya('Warning: Tile number inconsistency','a');
@@ -73,22 +73,30 @@ if((id==3)||(id==4))
         IT2(:,:,:,:,zz)=IT(:,:,(zz-1)*CC+1:(zz)*CC,:);
     end
     IT=IT2;clear IT2;
-    aux1=strfind(path,'_zstep');    
+    aux1=strfind(path,'_zstep');
     aux2=aux0(find(aux0>aux1,1,'first'));
 
     dad.zStepUm=str2num(path(aux1+6:aux2-1));
-    
+
 else
 
 end
 
 %   size(IT)
-%% ATENTION forcing to 32 bins when 30 or 31
+%% ATENTION forcing to 32 bins when 30 or 31 by adding last
 if(size(IT,3)==30)
- IT=cat(3,IT,zeros(size(IT,1),size(IT,2),2,size(IT,4),size(IT,5)));   
+    IT=cat(3,IT,IT(:,:,30,:,:));
 end
 if(size(IT,3)==31)
-IT=cat(3,IT,zeros(size(IT,1),size(IT,2),1,size(IT,4),size(IT,5)));
+    IT=cat(3,IT,IT(:,:,31,:,:));
+end
+%% if 31nB we have just extended the B, we gotta add a tail for the 30 to become 31
+if(strcmp(dad.configuration,'31nB'))
+    IT(:,:,31,:,:)=IT(:,:,30,:,:);
+end
+%% if 32Sp match 16th to 15th
+if(strcmp(dad.configuration,'32Sp'))
+IT(:,:,16,:,:)=IT(:,:,15,:,:);
 end
 % disp(size(IT,3));
 %%
