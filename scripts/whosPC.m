6ea226f7731e286f5a11a119276b8914fcd4367b
AlexVallmitjana <antares68@gmail.com>
2026-03-17 15:06:12 -0700

    v1.224

--- scripts/whosPC.m
+++ scripts/whosPC.m
@@ -13,29 +13,39 @@ if(nargin==0)
 end
 id=0;
 switch qui
-    case 'ALEX_PC'
-        qui='Alex';id=420;
-    case {'AFDURKIN-2020','AFDURKIN-26'}
-        qui='Amanda';id=18;
-    case {'LAPTOP-4SS4H9HR','BTORRADO-2025'}
-        qui='Belen';id=422;       
-    case 'SUMAN-BLI'
-        % qui='Suman';id=421;
-        qui='Amanda';id=18;
-    case 'wz'
-        qui='Wenyu';id=1237;
-    case 'QWFQFBBQFEBQEFBQEFBQEFB'
-        qui='Bruno';id=1238;
-    case 'DESKTOP-I0A9SVE'
-        qui='FLAME3';id=3;
     case 'MBF'
-        qui='FLAME2';id=2;
+        qui='FLAME2';
+        id=2;
+    case 'DESKTOP-I0A9SVE'
+        qui='FLAME3';
+        id=3;
+    case {'AFDURKIN-2020','AFDURKIN-26'}
+        qui='Amanda';
+        id=18;
+    case 'SUMAN-BLI'
+        qui='Amanda';
+        id=18;
+    case 'ALEX_PC'
+        qui='Alex';
+        id=420;
+    case {'LAPTOP-4SS4H9HR','BTORRADO-2025'}
+        qui='Belen';
+        id=422;       
     case 'DESKTOP-350VFI4'
-        qui='Navid';id=1234;
+        qui='Navid';
+        id=1234;
     case {'TYXY','YRYX_COMPUTER'}
-        qui='Yryx';id=1235;
+        qui='Yryx';
+        id=1235;
     case 'NLOM-BLI'
-        qui='NLOMlaptop';id=1236;		
+        qui='NLOMlaptop';
+        id=1236;		
+    case 'wz'
+        qui='Wenyu';
+        id=1237;
+    case 'QQ'
+        qui='Bruno';
+        id=1238;
     otherwise
 end
 
